4506cd5f5a209ae79e6e1d60d934e7254b1b73ce
Danyhug <danyhug@qq.com>
2024-06-05 23:08:12 +0800

    add

--- src/views/backend/ManagerView.vue
+++ src/views/backend/ManagerView.vue
@@ -1,29 +1,64 @@
 <template>
-  <el-table :data="articles" style="width: 100%;">
-    <el-table-column prop="id" label="编号" width="90" />
-    <el-table-column prop="title" label="文章标题" width="180" />
-    <el-table-column prop="categoryName" label="分类">
-      <template #default="scope">
-        <el-tag type="primary">{{ scope.row.categoryName }}</el-tag>
-      </template>
-    </el-table-column>
 
-    <el-table-column prop="tags" label="标签">
-      <template #default="scope">
-        <el-tag type="primary" v-for="tag in scope.row.tags" effect="plain" round>{{ tag.name }}</el-tag>
-      </template>
-    </el-table-column>
-    <el-table-column prop="views" label="浏览数" />
-    <el-table-column prop="wordNum" label="字数" />
-    <el-table-column prop="publishDate" label="发布时间" width="170" />
-    <el-table-column prop="updateDate" label="更新时间" width="170" />
-    <el-table-column fixed="right" label="操作" width="100">
-      <template #default="scope">
-        <el-button link type="primary" size="large" @click.prevent="edit(scope.row.id)">编辑</el-button>
-        <el-button link type="danger" size="small">删除</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+  <el-tabs v-model="activeName" type="border-card">
+    <el-tab-pane label="文章管理" name="first">
+
+      <el-table :data="articles" style="width: 100%;">
+        <el-table-column prop="id" label="编号" width="90" />
+        <el-table-column prop="title" label="文章标题" width="180" />
+        <el-table-column prop="categoryName" label="分类">
+          <template #default="scope">
+            <el-tag type="primary">{{ scope.row.categoryName }}</el-tag>
+          </template>
+        </el-table-column>
+
+        <el-table-column prop="tags" label="标签">
+          <template #default="scope">
+            <el-tag type="primary" v-for="tag in scope.row.tags" effect="plain" round>{{ tag.name }}</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column prop="views" label="浏览数" />
+        <el-table-column prop="wordNum" label="字数" />
+        <el-table-column prop="publishDate" label="发布时间" width="170" />
+        <el-table-column prop="updateDate" label="更新时间" width="170" />
+        <el-table-column fixed="right" label="操作" width="100">
+          <template #default="scope">
+            <el-button link type="primary" size="large" @click.prevent="edit(scope.row.id)">编辑</el-button>
+            <el-button link type="danger" size="small">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+    </el-tab-pane>
+    <el-tab-pane label="分类管理" name="second">
+
+      <el-table :data="categories">
+        <el-table-column prop="id" label="编号" width="90" />
+        <el-table-column prop="name" label="名称" width="90" />
+        <el-table-column prop="slug" label="编码名称" width="90" />
+        <el-table-column prop="createdAt" label="发布时间" width="170" />
+        <el-table-column prop="updatedAt" label="更新时间" width="170" />
+      </el-table>
+
+    </el-tab-pane>
+    <el-tab-pane label="标签管理" name="third">
+
+      <el-table :data="tags">
+        <el-table-column prop="id" label="编号" width="90" />
+        <el-table-column prop="name" label="名称" width="90" />
+        <el-table-column prop="slug" label="编码名称" width="90" />
+        <el-table-column prop="createdAt" label="发布时间" width="170" />
+        <el-table-column prop="updatedAt" label="更新时间" width="170" />
+      </el-table>
+
+
+    </el-tab-pane>
+  </el-tabs>
+
+
+
+
+
 </template>
 
 <script lang="ts" setup>
@@ -71,3 +106,8 @@ onMounted(() => {
   })
 })
 </script>
+<style scoped>
+.box-card {
+  width: 460px;
+}
+</style>
